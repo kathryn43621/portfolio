a888708524e76f2ded7ed7fac9812a9391a235aa
Kathryn <73561055+kathryn43621@users.noreply.github.com>
2024-10-01 16:59:29 +0100

    Added image for sharing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -41,6 +41,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <meta property="og:type" content="website" />
 
           <meta
             property="og:title"
