73b0f74b042188c9e401b31e7954a2666643adab
Kathryn <73561055+kathryn43621@users.noreply.github.com>
2024-10-01 17:49:01 +0100

    Added GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
-import Theme from '../styles/theme';
+import { GoogleTagManager } from "@next/third-parties/google";
+import Theme from "../styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -6,7 +7,7 @@ export default function App({ Component, pageProps }) {
       <Theme>
         <Component {...pageProps} />
       </Theme>
+      <GoogleTagManager gtmId="GTM-TDJCWTLX" />
     </>
   );
 }
- 
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -56,19 +56,7 @@ export default class MyDocument extends Document {
             content="https://portfolio-kathryn-yangs-projects.vercel.app/"
           />
 
-          {/* <!-- Google Tag Manager --> */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TDJCWTLX');`,
-            }}
-          />
-          {/* <!-- End Google Tag Manager --> */}
           <title>Yu Wei Yang | Portfolio</title>
-
           <link
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
@@ -80,16 +68,6 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-TDJCWTLX"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
