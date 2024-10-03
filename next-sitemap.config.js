408dd997aba0260aa048abd45ab289e79f150fd1
Kathryn <73561055+kathryn43621@users.noreply.github.com>
2024-10-03 15:48:19 +0100

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://portfolio-kathryn-yangs-projects.vercel.app/",
+  generateRobotsTxt: true,
+};
+
+console.log("Generating sitemap...");
+
+module.exports = config;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+
   async headers() {
     return [
       {
