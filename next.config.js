9d44bea47e34e9a790522ae04f07a1722767401c
Kathryn <73561055+kathryn43621@users.noreply.github.com>
2024-10-03 15:08:12 +0100

    Remove duplicated code

--- next.config.js
+++ next.config.js
@@ -1,37 +1,34 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          // Apply these headers to all routes in your application
-          source: '/(.*)', // Match all routes
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'public, max-age=31536000, immutable', // Caches assets for 1 year
-            },
-            {
-              key: 'Vary',
-              value: 'Accept-Encoding',
-            },
-          ],
-        },
-        {
-          // Specific API routes
-          source: '/api/(.*)',
-          headers: [
-            {
-              key: 'Cache-Control',
-              value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
 
-module.exports = nextConfig
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: "/(.*)", // Match all routes
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Caches assets for 1 year
+          },
+          {
+            key: "Vary",
+            value: "Accept-Encoding",
+          },
+        ],
+      },
+      {
+        // Specific API routes
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
