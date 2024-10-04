b8d2eb4a769fa5b5b51876f0266d9776c36e00f0
Kathryn <73561055+kathryn43621@users.noreply.github.com>
2024-10-04 12:30:32 +0100

    Set up GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,10 +4,10 @@ import Theme from "../styles/theme";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GoogleTagManager gtmId="GTM-TDJCWTLX" />
       <Theme>
         <Component {...pageProps} />
       </Theme>
-      <GoogleTagManager gtmId="GTM-TDJCWTLX" />
     </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import Acomplishments from "../components/Acomplishments/Acomplishments";
-import BgAnimation from "../components/BackgroundAnimation/BackgroundAnimation";
 import Contact from "../components/Contact/Contact";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
-import { Section } from "../styles/GlobalComponents";
 import Head from "next/head";
 
 const Home = () => {
